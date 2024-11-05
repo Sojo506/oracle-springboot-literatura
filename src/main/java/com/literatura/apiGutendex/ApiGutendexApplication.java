e32e09eab547f97db33b2e07a642c1f0e56d095a
Sojo506 <fsojodev@gmail.com>
2024-11-05 10:36:35 -0600

    init commit

---
+++ src/main/java/com/literatura/apiGutendex/ApiGutendexApplication.java
@@ -0,0 +1,13 @@
+package com.literatura.apiGutendex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGutendexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGutendexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/literatura/apiGutendex/ApiGutendexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.literatura.apiGutendex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGutendexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
