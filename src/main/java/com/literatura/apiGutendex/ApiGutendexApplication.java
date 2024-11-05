2c1c4412e0bc9f90fdb48b5ff159b9739081dd8f
Sojo506 <fsojodev@gmail.com>
2024-11-05 10:53:42 -0600

    adds main class to use it whenever runs the console

--- src/main/java/com/literatura/apiGutendex/ApiGutendexApplication.java
+++ src/main/java/com/literatura/apiGutendex/ApiGutendexApplication.java
@@ -1,5 +1,6 @@
 package com.literatura.apiGutendex;
 
+import com.literatura.apiGutendex.main.Main;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,12 +8,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ApiGutendexApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiGutendexApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGutendexApplication.class, args);
+    }
 
-	@Override
-	public void run(String... args) throws Exception {
-
-	}
+    @Override
+    public void run(String... args) throws Exception {
+        Main main = new Main();
+        main.start();
+    }
 }
