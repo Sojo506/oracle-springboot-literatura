71585d550cc8f25b664cde61483b3c34dc9b4962
Sojo506 <fsojodev@gmail.com>
2024-11-05 12:46:43 -0600

    adds attributes

--- src/main/java/com/literatura/apiGutendex/model/Author.java
+++ src/main/java/com/literatura/apiGutendex/model/Author.java
@@ -1,4 +1,22 @@
 package com.literatura.apiGutendex.model;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "author")
 public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private Integer birthYear;
+    private Integer deathYear;
+
+    @OneToMany(mappedBy = "author", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonManagedReference
+    private List<Book> books;
 }
--- src/main/java/com/literatura/apiGutendex/model/Book.java
+++ src/main/java/com/literatura/apiGutendex/model/Book.java
@@ -1,4 +1,25 @@
 package com.literatura.apiGutendex.model;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+
+
+@Entity
+@Table(name = "book")
 public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "author_id")
+    @JsonBackReference
+    private Author author;
+
+    private String language;
+    private Integer downloads;
+
+
 }
