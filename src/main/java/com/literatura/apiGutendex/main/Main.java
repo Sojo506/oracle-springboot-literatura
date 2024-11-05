15b6698541a9f0196bc5fb01c39c75b5452d638c
Sojo506 <fsojodev@gmail.com>
2024-11-05 10:53:03 -0600

    creates main class to start the console

---
+++ src/main/java/com/literatura/apiGutendex/main/Main.java
@@ -0,0 +1,11 @@
+package com.literatura.apiGutendex.main;
+
+import java.util.Scanner;
+
+public class Main {
+    private final Scanner sc = new Scanner(System.in);
+
+    public void start() {
+
+    }
+}
