e8fdb1d7794b755b4df37e159c645e29e9de47c5
Sojo506 <fsojodev@gmail.com>
2024-11-06 11:26:30 -0600

    adds constructor, and removes setters

--- src/main/java/com/literatura/apiGutendex/model/Author.java
+++ src/main/java/com/literatura/apiGutendex/model/Author.java
@@ -20,43 +20,29 @@ public class Author {
     @JsonManagedReference
     private List<Book> books;
 
-    public Long getId() {
-        return id;
+    public Author(String name, Integer birthYear, Integer deathYear) {
+        this.name = name;
+        this.birthYear = birthYear;
+        this.deathYear = deathYear;
     }
 
-    public void setId(Long id) {
-        this.id = id;
+    public Long getId() {
+        return id;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public Integer getBirthYear() {
         return birthYear;
-    }
-
-    public void setBirthYear(Integer birthYear) {
-        this.birthYear = birthYear;
     }
 
     public Integer getDeathYear() {
         return deathYear;
     }
 
-    public void setDeathYear(Integer deathYear) {
-        this.deathYear = deathYear;
-    }
-
     public List<Book> getBooks() {
         return books;
-    }
-
-    public void setBooks(List<Book> books) {
-        this.books = books;
     }
 }
