2ac1afdb6017c8b0b4ea2792f7f93ca9d3e87481
Sojo506 <fsojodev@gmail.com>
2024-11-06 11:10:41 -0600

    creates records to map books data and authors data

---
+++ src/main/java/com/literatura/apiGutendex/model/AuthorData.java
@@ -0,0 +1,8 @@
+package com.literatura.apiGutendex.model;
+
+public record AuthorData(
+        String name,
+        Integer birth_year,
+        Integer death_year
+) {
+}
---
+++ src/main/java/com/literatura/apiGutendex/model/BookData.java
@@ -0,0 +1,14 @@
+package com.literatura.apiGutendex.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record BookData(
+        String title,
+        List<AuthorData> authors,
+        List<String> languages,
+        Integer download_count
+) {
+}
