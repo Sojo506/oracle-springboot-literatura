4c9826e3b7968b68a27663b1532ea7c716138ea6
Sojo506 <fsojodev@gmail.com>
2024-11-06 23:55:39 -0600

    removes id from toString

--- src/main/java/com/literatura/apiGutendex/model/Book.java
+++ src/main/java/com/literatura/apiGutendex/model/Book.java
@@ -54,7 +54,6 @@ public class Book {
     @Override
     public String toString() {
         return "Book{" +
-                "id=" + id +
                 ", title='" + title + '\'' +
                 ", author=" + author +
                 ", language='" + language + '\'' +
