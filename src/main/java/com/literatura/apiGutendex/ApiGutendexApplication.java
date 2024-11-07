67c8b6fd27373543ee7adff8cf074fab096f29e3
Sojo506 <fsojodev@gmail.com>
2024-11-06 23:56:36 -0600

    injects dependencies: IBookRepository and IAuthorRepository

--- src/main/java/com/literatura/apiGutendex/ApiGutendexApplication.java
+++ src/main/java/com/literatura/apiGutendex/ApiGutendexApplication.java
@@ -1,6 +1,9 @@
 package com.literatura.apiGutendex;
 
 import com.literatura.apiGutendex.main.Main;
+import com.literatura.apiGutendex.repository.IAuthorRepository;
+import com.literatura.apiGutendex.repository.IBookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,13 +11,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ApiGutendexApplication implements CommandLineRunner {
 
+    @Autowired
+    private IBookRepository bookRepository;
+
+    @Autowired
+    private IAuthorRepository authorRepository;
+
     public static void main(String[] args) {
         SpringApplication.run(ApiGutendexApplication.class, args);
     }
 
     @Override
     public void run(String... args) throws Exception {
-        Main main = new Main();
+        Main main = new Main(bookRepository, authorRepository);
         main.start();
     }
 }
