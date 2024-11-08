8cbb4e993f6ece2f217c6b1647f903c751f94a33
Sojo506 <fsojodev@gmail.com>
2024-11-07 23:27:12 -0600

    adds query to find by name, birth year, and death year

--- src/main/java/com/literatura/apiGutendex/repository/IAuthorRepository.java
+++ src/main/java/com/literatura/apiGutendex/repository/IAuthorRepository.java
@@ -2,6 +2,13 @@ package com.literatura.apiGutendex.repository;
 
 import com.literatura.apiGutendex.model.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface IAuthorRepository extends JpaRepository<Author, Long> {
+
+    Optional<Author> findByNameAndBirthYearAndDeathYear(String name, Integer birthYear, Integer deathYear);
+
 }
