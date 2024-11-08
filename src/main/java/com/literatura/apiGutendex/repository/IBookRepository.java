f95a0e3a66ea3dd2ab4be870ee5ab748abf0f8e5
Sojo506 <fsojodev@gmail.com>
2024-11-08 00:30:07 -0600

    adds query to find books by a given language

--- src/main/java/com/literatura/apiGutendex/repository/IBookRepository.java
+++ src/main/java/com/literatura/apiGutendex/repository/IBookRepository.java
@@ -3,10 +3,12 @@ package com.literatura.apiGutendex.repository;
 import com.literatura.apiGutendex.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface IBookRepository extends JpaRepository<Book, Long> {
 
     Optional<Book> findByTitle(String title);
+    List<Book> findByLanguage(String lang);
 
 }
