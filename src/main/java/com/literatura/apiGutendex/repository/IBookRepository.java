72639c7d8b8422b5b154d1ff4268ec2e5a56d7d0
Sojo506 <fsojodev@gmail.com>
2024-11-07 23:24:22 -0600

    adds query to find by title

--- src/main/java/com/literatura/apiGutendex/repository/IBookRepository.java
+++ src/main/java/com/literatura/apiGutendex/repository/IBookRepository.java
@@ -3,5 +3,10 @@ package com.literatura.apiGutendex.repository;
 import com.literatura.apiGutendex.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface IBookRepository extends JpaRepository<Book, Long> {
+
+    Optional<Book> findByTitle(String title);
+
 }
