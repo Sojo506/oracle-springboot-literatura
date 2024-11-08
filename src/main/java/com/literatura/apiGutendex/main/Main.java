557bc29d0ee06708fad9c660e93269a84fa6ccd8
Sojo506 <fsojodev@gmail.com>
2024-11-08 11:55:08 -0600

    code refactored v01

--- src/main/java/com/literatura/apiGutendex/main/Main.java
+++ src/main/java/com/literatura/apiGutendex/main/Main.java
@@ -9,6 +9,9 @@ import com.literatura.apiGutendex.repository.IBookRepository;
 import com.literatura.apiGutendex.service.ConvertData;
 import com.literatura.apiGutendex.service.FetchApi;
 
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.*;
 
 /* TO DO
@@ -24,10 +27,10 @@ public class Main {
     private final String URL = "https://gutendex.com/books/?search=";
     private final FetchApi fetch = new FetchApi();
     private final ConvertData convertData = new ConvertData();
+    private final IBookRepository bookRepository;
+    private final IAuthorRepository authorRepository;
     List<Book> books;
     List<Author> authors;
-    private IBookRepository bookRepository;
-    private IAuthorRepository authorRepository;
 
     public Main(IBookRepository bookRepository, IAuthorRepository authorRepository) {
         this.bookRepository = bookRepository;
@@ -61,138 +64,19 @@ public class Main {
 
             switch (opt) {
                 case 1:
-                    System.out.print("Enter the book's name you want to search for: ");
-                    String bookTitle = sc.nextLine().toLowerCase().replace(" ", "%20");
-
-                    // search book in the db
-                    Optional<Book> existingBook = bookRepository.findAll().stream()
-                            .filter(b -> b.getTitle().toLowerCase().contains(bookTitle))
-                            .findFirst();
-
-                    if (existingBook.isPresent()) {
-                        System.out.println("Book already exists in the database: " + existingBook.get());
-                    } else {
-                        // Fetch data from API si no está en la base de datos
-                        String json = fetch.fetchApi(URL + bookTitle);
-                        List<BookData> data = convertData.convertData(json, Data.class).bookDataList();
-
-                        if (data.isEmpty()) {
-                            System.out.println("Not found in the API");
-                            break;
-                        }
-
-                        // Find the book by title in the API response
-                        Optional<BookData> auxBook = data.stream()
-                                .filter(b -> containsTitle(b.title(), bookTitle))
-                                .findFirst();
-
-                        if (auxBook.isEmpty()) {
-                            System.out.println("Not found in the API");
-                            break;
-                        }
-
-                        Book book = new Book(auxBook.get());
-
-                        // create or find author, then associate it with the book
-                        Author author = new Author(
-                                book.getAuthor().getName(),
-                                book.getAuthor().getBirthYear(),
-                                book.getAuthor().getDeathYear()
-                        );
-
-                        authorRepository.findByNameAndBirthYearAndDeathYear(
-                                author.getName(), author.getBirthYear(), author.getDeathYear()
-                        ).ifPresentOrElse(existingAuthor -> {
-                            book.setAuthor(existingAuthor);
-                        }, () -> {
-                            authorRepository.save(author);
-                            book.setAuthor(author);
-                        });
-
-                        // save book in the db
-                        bookRepository.save(book);
-                        System.out.println("Book added to the database: " + book);
-                    }
+                    searchBookByTitle();
                     break;
                 case 2:
-                    books = bookRepository.findAll();
-
-                    books.stream()
-                            .sorted(Comparator.comparing(Book::getTitle))
-                            .forEach(b -> {
-                                System.out.println("\n----- Book -----");
-                                System.out.println("Title: " + b.getTitle());
-                                System.out.println("Author: " + b.getAuthor().getName());
-                                System.out.println("Language: " + b.getLanguage());
-                                System.out.println("Downloads: " + b.getDownloads());
-                                System.out.println("----- ---- -----");
-                            });
+                    listRegisteredBooks();
                     break;
                 case 3:
-                    authors = authorRepository.findAll();
-
-                    authors.stream()
-                            .forEach(a -> {
-                                System.out.println("\n----- Author -----");
-                                System.out.println("Name: " + a.getName());
-                                System.out.println("Birth day: " + a.getBirthYear());
-                                System.out.println("Death year: " + a.getDeathYear());
-                                System.out.print("Books: ");
-                                List<String> titleList = a.getBooks().stream().map(Book::getTitle).toList();
-                                System.out.print(String.join(", ", titleList));
-
-                                System.out.println("\n----- ---- -----");
-                            });
+                    listRegisteredAuthors();
                     break;
                 case 4:
-                    System.out.print("Enter the year alive of the author you want to search: ");
-                    Integer year = sc.nextInt();
-                    sc.nextLine();
-
-                    authors = authorRepository.findAuthorsByGivenYear(year);
-
-                    if (authors.isEmpty()) {
-                        System.out.println("Authors not found");
-                    } else {
-                        authors.stream()
-                                .forEach(a -> {
-                                    System.out.println();
-                                    System.out.println("Name: " + a.getName());
-                                    System.out.println("Birth day: " + a.getBirthYear());
-                                    System.out.println("Death year: " + a.getDeathYear());
-                                    System.out.print("Books: ");
-                                    List<String> titleList = a.getBooks().stream().map(Book::getTitle).toList();
-                                    System.out.print(String.join(", ", titleList));
-                                    System.out.println();
-                                });
-                    }
+                    listAuthorsByYear();
                     break;
                 case 5:
-                    System.out.print("Enter the language to search for the books: ");
-                    System.out.println("""
-                            es - spanish
-                            en - english
-                            fr - france
-                            pt - portuguese
-                            """);
-                    var searchBookByLang = sc.nextLine().toLowerCase();
-
-                    List<Book> booksByLang = bookRepository.findByLanguage(searchBookByLang);
-
-                    if (booksByLang.isEmpty()) {
-                        System.out.println("Books not found");
-                    } else {
-                        booksByLang.stream()
-                                .sorted(Comparator.comparing(Book::getTitle))
-                                .forEach(b -> {
-                                    System.out.println("\n----- Book -----");
-                                    System.out.println("Title: " + b.getTitle());
-                                    System.out.println("Author: " + b.getAuthor().getName());
-                                    System.out.println("Language: " + b.getLanguage());
-                                    System.out.println("Downloads: " + b.getDownloads());
-                                    System.out.println("----- ---- -----");
-                                });
-                    }
+                    listBooksByLanguage();
                     break;
                 case 0:
                     System.out.println("Closing the app...");
@@ -205,14 +89,166 @@ public class Main {
 
     }
 
-    private boolean containsTitle(String title, String searchedTitle) {
-        List<String> words = List.of(title.toLowerCase().split("\\s"));
+    private void searchBookByTitle() {
+        System.out.print("Enter the book's name you want to search for: ");
+        String bookTitle = sc.nextLine().toLowerCase();
 
-        for (String word : words) {
-            if (word.equals(searchedTitle.toLowerCase())) {
-                return true;
+        if (!isValidTitle(bookTitle)) {
+            System.out.println("\nYour search must be greater than 3 characters!");
+        } else {
+            Optional<Book> existingBook = findBookInDatabase(bookTitle);
+            if (existingBook.isPresent()) {
+                System.out.println("\nBook already exists in the database: " + existingBook.get());
+            } else {
+                fetchAndAddBookFromApi(encodeForUrl(bookTitle));
             }
         }
-        return false;
+    }
+
+    private Optional<Book> findBookInDatabase(String bookTitle) {
+        return bookRepository.findAll().stream()
+                .filter(b -> b.getTitle().toLowerCase().contains(bookTitle))
+                .findFirst();
+    }
+
+    private void fetchAndAddBookFromApi(String bookTitle) {
+        String json = fetch.fetchApi(URL + bookTitle);
+        List<BookData> data = convertData.convertData(json, Data.class).bookDataList();
+
+        if (data.isEmpty()) {
+            System.out.println("\nNot found in the API");
+            return;
+        }
+
+        Optional<BookData> auxBook = data.stream()
+                .filter(b -> containsTitle(b.title(), decodeForUrl(bookTitle)))
+                .findFirst();
+
+        if (auxBook.isEmpty()) {
+            System.out.println("\nNot found in the API");
+            return;
+        }
+
+        Book book = new Book(auxBook.get());
+        associateAndSaveBook(book);
+        System.out.println("\nBook added to the database: " + book);
+    }
+
+    private void associateAndSaveBook(Book book) {
+        Author author = new Author(
+                book.getAuthor().getName(),
+                book.getAuthor().getBirthYear(),
+                book.getAuthor().getDeathYear()
+        );
+
+        authorRepository.findByNameAndBirthYearAndDeathYear(
+                author.getName(), author.getBirthYear(), author.getDeathYear()
+        ).ifPresentOrElse(
+//                before
+//                existingAuthor -> book.setAuthor(existingAuthor),
+//                now
+                book::setAuthor,
+                () -> {
+                    authorRepository.save(author);
+                    book.setAuthor(author);
+                }
+        );
+
+        bookRepository.save(book);
+    }
+
+    private void listRegisteredBooks() {
+        books = bookRepository.findAll();
+        displayBooks(books);
+    }
+
+    private void listRegisteredAuthors() {
+        authors = authorRepository.findAll();
+        displayAuthors(authors);
+    }
+
+    private void listAuthorsByYear() {
+        System.out.print("Enter the year alive of the author you want to search: ");
+        int year = 0;
+
+        try {
+            year = sc.nextInt();
+
+        } catch (InputMismatchException e) {
+            System.out.println(e.getMessage());
+        }
+        sc.nextLine();
+
+        authors = authorRepository.findAuthorsByGivenYear(year);
+
+        if (authors.isEmpty()) {
+            System.out.println("\nAuthors not found");
+        } else {
+            displayAuthors(authors);
+        }
+    }
+
+    private void listBooksByLanguage() {
+        System.out.println("\nEnter the language to search for the books: ");
+        System.out.println("""
+                es - Spanish
+                en - English
+                fr - French
+                pt - Portuguese
+                """);
+        String language = sc.nextLine().toLowerCase();
+
+        List<Book> booksByLang = bookRepository.findByLanguage(language);
+        if (booksByLang.isEmpty()) {
+            System.out.println("Books not found");
+        } else {
+            displayBooks(booksByLang);
+        }
+    }
+
+    private boolean containsTitle(String title, String searchedTitle) {
+        String normalizedTitle = title.toLowerCase();
+        String normalizedSearchedTitle = searchedTitle.toLowerCase();
+
+        return normalizedTitle.contains(normalizedSearchedTitle);
+    }
+
+    private void displayAuthors(List<Author> authors) {
+        authors
+                .forEach(a -> {
+                    System.out.println("\n----- Book -----");
+                    System.out.println("Name: " + a.getName());
+                    System.out.println("Birth day: " + a.getBirthYear());
+                    System.out.println("Death year: " + a.getDeathYear());
+                    System.out.print("Books: ");
+                    List<String> titleList = a.getBooks().stream().map(Book::getTitle).toList();
+                    System.out.print(String.join(", ", titleList));
+                    System.out.println("\n----- ---- -----");
+                });
+    }
+
+    private void displayBooks(List<Book> books) {
+        books.stream()
+                .sorted(Comparator.comparing(Book::getTitle))
+                .forEach(b -> {
+                    System.out.println("\n----- Book -----");
+                    System.out.println("Title: " + b.getTitle());
+                    System.out.println("Author: " + b.getAuthor().getName());
+                    System.out.println("Language: " + b.getLanguage());
+                    System.out.println("Downloads: " + b.getDownloads());
+                    System.out.println("----- ---- -----");
+                });
+    }
+
+    private boolean isValidTitle(String title) {
+        return title.length() >= 3;
+    }
+
+    private String encodeForUrl(String title) {
+        return URLEncoder.encode(title, StandardCharsets.UTF_8);
+    }
+
+    private String decodeForUrl(String title) {
+        return URLDecoder.decode(title, StandardCharsets.UTF_8);
     }
 }
